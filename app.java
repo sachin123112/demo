ec539e9bab93497b9b9ecf6473d6366f85192ae9
sachin123112 <songforswag@gmail.com>
2025-12-03 16:46:33 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,5 +2,7 @@ class app {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("sachin");
+        
+        System.out.println("http://13.233.168.136:8686/job/test/");
     }
 }
