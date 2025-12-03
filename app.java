85a807ea2f23c2778f1ad1a948145186796b0f6c
sachin123112 <songforswag@gmail.com>
2025-12-03 15:39:08 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,9 +1,1 @@
-public class AddNumbers {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-
-        int sum = a + b;
-        System.out.println("Sum: " + sum);
-    }
-}
+system.out.println("hello");
